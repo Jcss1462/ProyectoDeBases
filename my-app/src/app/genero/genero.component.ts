3b457f8dc0eba59ad68227d82dd4a5f9aff5109f
Juan Camilo Salazar Serna <jcss1462@gmail.com>
2019-04-04 02:40:04 -0500

    generoAgregado

--- my-app/src/app/genero/genero.component.ts
+++ my-app/src/app/genero/genero.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+//importacion par obtener la ultima pagina visitada
+import { Location } from '@angular/common';
+
 @Component({
   selector: 'app-genero',
   templateUrl: './genero.component.html',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GeneroComponent implements OnInit {
 
-  constructor() { }
+  constructor(private location: Location) { }
 
   ngOnInit() {
+  }
+
+  //activar biografia
+  activarBiografia(): void {
+    let activador= document.getElementById("listabiografia");
+    activador.classList.toggle('active');
+  }
+
+  
+  //activar lista de artista
+  activarArtista(): void {
+    let activador= document.getElementById("listaArista");
+    activador.classList.toggle('active');
+  }
+
+  
+  //activar lista de canciones
+  activarCancion(): void {
+    let activador= document.getElementById("listaCanciones");
+    activador.classList.toggle('active');
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
